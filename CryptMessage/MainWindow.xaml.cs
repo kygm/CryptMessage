10bd4e9d4b6888b68c604d55bf094207e5dee6a2
Kenrichk <sg6157ye@go.minnstate.edu>
2021-04-02 18:01:52 -0500

    V 1.05

--- CryptMessage/MainWindow.xaml.cs
+++ CryptMessage/MainWindow.xaml.cs
@@ -19,9 +19,52 @@ namespace CryptMessage
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool home = true;//this var and others similar will hopefully be used to turn on and off elements belonging to different pages. 
+        string page = "home";
+        System.Windows.Visibility visible = Visibility.Visible;
+        System.Windows.Visibility invisible = Visibility.Hidden;
+
+
+        
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void mnuAbout(object sender, RoutedEventArgs e)
+        {
+            page = "about";
+            updatePages(page);
+        }
+        private void updatePages(String page)
+        {
+            homeVis(page);
+            aboutVis(page);
+        }
+        private void homeVis(String page)
+        {//set the visibilty for each page in its own function, run if statement on the page name, adjust visibility accordingly.
+            System.Windows.Visibility visState;
+            if (page == "home")
+            {
+                visState = visible;
+            }
+            else { visState = invisible; }
+
+        }
+        private void aboutVis(String page)
+        {
+            System.Windows.Visibility visState;
+            if (page == "about") {
+                visState = visible;
+            }
+            else { visState = invisible; }
+            About1.Visibility = visState;
+        }
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            testLabel.Visibility = visible;
         }
     }
 }
