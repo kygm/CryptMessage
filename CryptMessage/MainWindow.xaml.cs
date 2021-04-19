c4cd5a1d26e9013e390f82cb80513c952da12331
Kenrichk <sg6157ye@go.minnstate.edu>
2021-04-18 20:54:09 -0500

    V 1.17

--- CryptMessage/MainWindow.xaml.cs
+++ CryptMessage/MainWindow.xaml.cs
@@ -51,21 +51,6 @@ namespace CryptMessage
         string page = "";
         System.Windows.Visibility visible = Visibility.Visible;
         System.Windows.Visibility invisible = Visibility.Hidden;
-
-        private static async Task<char> Main()
-        {
-
-            try
-            {
-                string responseBody = await client.GetStringAsync("https://crypt-message.herokuapp.com/");
-                Console.WriteLine(responseBody);
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!\nMessage: {0}", e.Message);
-            }
-            return 'a';
-        }
         public MainWindow()
         {
             client.BaseAddress = server;
@@ -73,12 +58,17 @@ namespace CryptMessage
             page = "login";
             updatePages(page);
         }
+
+
+        #region Page Functions       
         private void updatePages(String page)
         {
             bool menuVis;
-            if (page == "login"){ 
-                menuVis = false; 
-            } else { menuVis = true; }
+            if (page == "login")
+            {
+                menuVis = false;
+            }
+            else { menuVis = true; }
             SettingsMnu.IsEnabled = menuVis;
             HomeMnu.IsEnabled = menuVis;
             ConversationsMnu.IsEnabled = menuVis;
@@ -89,8 +79,129 @@ namespace CryptMessage
             settingsVis(page);
             loginVis(page);
         }
+        private void homeVis(String page)
+            {//set the visibilty for each page in its own function, run if statement on the page name, adjust visibility accordingly.
+                System.Windows.Visibility visState;
+                if (page == "home")
+                {
+                    visState = visible;
+                }
+                else { visState = invisible; }
+                ConvoList.Visibility = visState;
+                MsgTxtBox.Visibility = visState;
+                MsgSendBtn.Visibility = visState;
+                UsernameDisplayLbl.Visibility = visState;
+                SendToLbl.Visibility = visState;
+                SendToTxt.Visibility = visState;
+            }
+            private void aboutVis(String page)
+            {
+                System.Windows.Visibility visState;
+                if (page == "about")
+                {
+                    visState = visible;
+                }
+                else { visState = invisible; }
+                About1.Visibility = visState;
+            }
+            private void convoManageVis(String page)
+            {
+                System.Windows.Visibility visState;
+                if (page == "conversations")
+                {
+                    visState = visible;
+                }
+                else { visState = invisible; }
+                AddConvoBtn.Visibility = visState;
+                NewConvoCreateBtn.Visibility = visState;
+                DelConvoBtn.Visibility = visState;
+            }
+            private void settingsVis(String page) {
+                System.Windows.Visibility visState;
+                if (page == "settings")
+                {
+                    visState = visible;
+                }
+                else { visState = invisible; }
+            }
+            private void newConvoVis(object sender, RoutedEventArgs e)
+            {
+                System.Windows.Visibility visState = NewConvoTitleLbl.Visibility;
+                if (visState == invisible)
+                {
+                    visState = visible;
+                }
+                else if (visState == visible)
+                {
+                    visState = invisible;
+                }
+                NewConvoTitleLbl.Visibility = visState;
+                NewConvoTitleTxtBox.Visibility = visState;
+                NewRecipientLbl.Visibility = visState;
+                NewRecipientTxtBox.Visibility = visState;
+            }
+            private void loginVis(string page)
+            {
+                System.Windows.Visibility visState;
+                if (page == "login")
+                {
+                    visState = visible;
+                }
+                else { visState = invisible; }
+                usernameLbl.Visibility = visState;
+                usernameTxtBox.Visibility = visState;
+                loginPasswordLbl.Visibility = visState;
+                loginPassBox.Visibility = visState;
+                LoginBtn.Visibility = visState;
+                debugLbl.Visibility = visState;
+                LoginStatusLbl.Visibility = visState;
+            }
+        #endregion
 
-        //page switching functions
+        #region Window Privacy Functions
+        private void Window_DragEnter(object sender, DragEventArgs e)//prevent copy paste
+            {
+                var pageView = "blank";
+                updatePages(pageView);
+            }
+            private void Window_MouseLeave(object sender, MouseEventArgs e)
+            {
+                var pageView = "blank";
+                updatePages(pageView);
+            }
+            private void Window_Deactivated(object sender, EventArgs e)
+            {
+                var pageView = "blank";
+                updatePages(pageView);
+            }
+            private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)
+                    || Keyboard.IsKeyDown(Key.LWin) || Keyboard.IsKeyDown(Key.RWin)
+                    || Keyboard.IsKeyDown(Key.Print) || Keyboard.IsKeyDown(Key.PrintScreen)
+                    || Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt))
+                {
+                    var pageView = "blank";
+                    updatePages(pageView);
+                }
+            }
+            private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+            {
+            
+                if (Keyboard.IsKeyUp(Key.LeftCtrl) || Keyboard.IsKeyUp(Key.RightCtrl)
+                    || Keyboard.IsKeyUp(Key.LWin) || Keyboard.IsKeyUp(Key.RWin)
+                    || Keyboard.IsKeyUp(Key.Print) || Keyboard.IsKeyUp(Key.PrintScreen)
+                    || Keyboard.IsKeyUp(Key.LeftAlt) || Keyboard.IsKeyUp(Key.RightAlt))
+                {
+                    updatePages(page);
+                }
+            }
+            private void Window_MouseEnter(object sender, MouseEventArgs e){ updatePages(page); }
+            private void Window_Activated(object sender, EventArgs e) { updatePages(page); }
+            private void Window_Drop(object sender, DragEventArgs e) { updatePages(page); }
+        #endregion
+
+        //UI control functions
         private void SettingsMnu_Click(object sender, RoutedEventArgs e)
         {
             page = "settings";
@@ -111,150 +222,32 @@ namespace CryptMessage
             page = "about";
             updatePages(page);
         }
-        
-        private void homeVis(String page)
-        {//set the visibilty for each page in its own function, run if statement on the page name, adjust visibility accordingly.
-            System.Windows.Visibility visState;
-            if (page == "home")
-            {
-                visState = visible;
-            }
-            else { visState = invisible; }
-            ConvoList.Visibility = visState;
-            MsgTxtBox.Visibility = visState;
-            MsgSendBtn.Visibility = visState;
-            UsernameDisplayLbl.Visibility = visState;
-            SendToLbl.Visibility = visState;
-            SendToTxt.Visibility = visState;
-        }
-        private void aboutVis(String page)
-        {
-            System.Windows.Visibility visState;
-            if (page == "about")
-            {
-                visState = visible;
-            }
-            else { visState = invisible; }
-            About1.Visibility = visState;
-        }
-        private void convoManageVis(String page)
-        {
-            System.Windows.Visibility visState;
-            if (page == "conversations")
-            {
-                visState = visible;
-            }
-            else { visState = invisible; }
-            AddConvoBtn.Visibility = visState;
-            NewConvoCreateBtn.Visibility = visState;
-            DelConvoBtn.Visibility = visState;
-        }
-        private void settingsVis(String page) {
-            System.Windows.Visibility visState;
-            if (page == "settings")
-            {
-                visState = visible;
-            }
-            else { visState = invisible; }
-        }
-        private void newConvoVis(object sender, RoutedEventArgs e)
-        {
-            System.Windows.Visibility visState = NewConvoTitleLbl.Visibility;
-            if (visState == invisible)
-            {
-                visState = visible;
-            }
-            else if (visState == visible)
-            {
-                visState = invisible;
-            }
-            NewConvoTitleLbl.Visibility = visState;
-            NewConvoTitleTxtBox.Visibility = visState;
-            NewRecipientLbl.Visibility = visState;
-            NewRecipientTxtBox.Visibility = visState;
-        }
-        private void loginVis(string page)
-        {
-            System.Windows.Visibility visState;
-            if (page == "login")
-            {
-                visState = visible;
-            }
-            else { visState = invisible; }
-            usernameLbl.Visibility = visState;
-            usernameTxtBox.Visibility = visState;
-            loginPasswordLbl.Visibility = visState;
-            loginPassBox.Visibility = visState;
-            LoginBtn.Visibility = visState;
-        }
-
-        //window privacy functions
-        private void Window_DragEnter(object sender, DragEventArgs e)//prevent copy paste
-        {
-            var pageView = "blank";
-            updatePages(pageView);
-        }
-        private void Window_MouseLeave(object sender, MouseEventArgs e)
-        {
-            var pageView = "blank";
-            updatePages(pageView);
-        }
-        private void Window_Deactivated(object sender, EventArgs e)
-        {
-            var pageView = "blank";
-            updatePages(pageView);
-        }
-        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)
-                || Keyboard.IsKeyDown(Key.LWin) || Keyboard.IsKeyDown(Key.RWin)
-                || Keyboard.IsKeyDown(Key.Print) || Keyboard.IsKeyDown(Key.PrintScreen)
-                || Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt))
-            {
-                var pageView = "blank";
-                updatePages(pageView);
-            }
-        }
-        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            
-            if (Keyboard.IsKeyUp(Key.LeftCtrl) || Keyboard.IsKeyUp(Key.RightCtrl)
-                || Keyboard.IsKeyUp(Key.LWin) || Keyboard.IsKeyUp(Key.RWin)
-                || Keyboard.IsKeyUp(Key.Print) || Keyboard.IsKeyUp(Key.PrintScreen)
-                || Keyboard.IsKeyUp(Key.LeftAlt) || Keyboard.IsKeyUp(Key.RightAlt))
-            {
-                updatePages(page);
-            }
-        }
-        private void Window_MouseEnter(object sender, MouseEventArgs e){ updatePages(page); }
-        private void Window_Activated(object sender, EventArgs e) { updatePages(page); }
-        private void Window_Drop(object sender, DragEventArgs e) { updatePages(page); }
-
-        //UI control functions
         private async void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
             string username = usernameTxtBox.Text;
             string password = loginPassBox.Password.ToString();
-            var user = new User(username, password);
+            User user = new User(username, password);
 
-            var json = JsonConvert.SerializeObject(user);
+            string json = JsonConvert.SerializeObject(user);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
 
 
             var res = await client.PostAsync(url + "login", data);
             Console.WriteLine(user.ToString());
 
-            var body = res.Content.ReadAsStringAsync().Result;
-            debugLabel.Content = body.ToString();
+            string body = res.Content.ReadAsStringAsync().Result;
+            debugLbl.Content = body.ToString();
             Console.WriteLine(body.ToString());
+
+            Console.WriteLine(body);
 
             bool auth;
 
 
-            if (body.Equals("Yes"))
+            if (body.Equals("\"Yes\""))
             {
                 //setting visibiilty status to invisible for login status
-                LblLoginStatus.Visibility = invisible;
+                LoginStatusLbl.Visibility = invisible;
                 Console.WriteLine("User authenticated");
                 page = "home";
                 updatePages(page);
@@ -262,15 +255,15 @@ namespace CryptMessage
             }
             else if (body.Equals("No"))
             {
-                LblLoginStatus.Content = "Incorrect Password!";
+                LoginStatusLbl.Content = "Incorrect Password!";
             }
             else if(body.ToString() == "No user found!")
             {
-                LblLoginStatus.Content = "No User Found!";
+                LoginStatusLbl.Content = "No User Found!";
             }
             else
             {
-                LblLoginStatus.Content = "Server Error!";
+                LoginStatusLbl.Content = "Server Error!";
             }
             
            
